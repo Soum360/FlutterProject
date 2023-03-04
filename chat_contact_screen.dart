d1d43b24a1c39dc8d684c7789b4a510648ffaa34
Soum360 <101164733+Soum360@users.noreply.github.com>
2023-03-04 11:52:22 +0100

    Add files via upload

---
+++ chat_contact_screen.dart
@@ -0,0 +1,15 @@
+class ChatContacts extends StatefulWidget {
+  const ChatContacts({ Key? key }) : super(key: key);
+
+  @override
+  _ChatContactsState createState() => _ChatContactsState();
+}
+
+class _ChatContactsState extends State<ChatContacts> {
+  @override
+  Widget build(BuildContext context) {
+    return (
+      
+    );
+  }
+}
